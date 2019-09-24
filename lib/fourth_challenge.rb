8703c6932b4b515243a28bf93083c1bb75a8f0d2
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2019-09-24 14:33:49 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,9 +5,13 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        {name: Benvolio,
+        {name: "Benvolio",
          age: "17",
          attitude: "worried"
+        },
+        name: "Mercutio",
+         age: "18",
+         attitude: "hot-headed"
         }
       ]
    }, 
@@ -16,7 +20,14 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+        name: "Steven",
+         age: "30",
+         attitude: "confused"
+        },
+        name: "Nurse",
+         age: "44",
+         attitude: "worried"
+        }
       ]
    }
 }
